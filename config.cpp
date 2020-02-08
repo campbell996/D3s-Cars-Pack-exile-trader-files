e78304fd7e3180cb3280f6f354025c217a0c99b1
campbell996 <60759285+campbell996@users.noreply.github.com>
2020-02-08 19:26:11 +1100

    Update config.cpp

--- config.cpp
+++ config.cpp
@@ -1,49 +1,19 @@
-class CfgExileArsenal
-{
-	#include "traders\D3SV\ItemListD3SV.hpp"
-	class Exile_Uniform_BambiOverall				{ quality = 1; price = 1; sellPrice = 1; };
+Find...    class CfgExileArsenal
+Add....	   #include "traders\D3SV\ItemListD3SV.hpp"
 	
 	
 	
-class CfgTraderCategories
-{
-	#include "traders\D3SV\ItemListD3SV.hpp"
-	class Community
-	{
-		name = "Community Items";
-		icon = "a3\ui_f\data\gui\Rsc\RscDisplayArsenal\uniform_ca.paa";
-		items[] =
-		{
-			// Add your items here <3
-		};
-	};
+	
+Find...	   class CfgTraderCategories
+Add....    #include "traders\D3SV\ItemListD3SV.hpp"
+
 	
 	
 	
-class Exile_Trader_Vehicle
-	{
-		name = "VEHICLE";
-		showWeaponFilter = 0;
-		categories[] = 
-		{
-			"Cars",
-			"Trucks",
-			"D3SCars",
-			"D3SBikes"
-		};
-	};
-	
-	/**
-	 * Sells choppers and planes
-	 */
-	class Exile_Trader_Aircraft
-	{
-		name = "AIRCRAFT";
-		showWeaponFilter = 0;
-		categories[] = 
-		{
-			"Choppers",
-			"Planes",
-			"D3SChoppers"
-		};
-	};
+Find...    class CfgTraders
+Find...    class Exile_Trader_Vehicle
+Add....	   "D3SCars",
+Add....	   "D3SBikes"
+		
+Find...    class Exile_Trader_Aircraft
+Add....	   "D3SChoppers"
